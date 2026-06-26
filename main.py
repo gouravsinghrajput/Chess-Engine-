07cd6d56482d8a428189953ba9bc814262a22f48
Gourav Singh Rajput <gsrajput24sept@gmail.com>
2026-06-26 11:46:59 +0530

    main setup for the board

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 #  this contains the main function 
+
+import pygame as pg 
+import numpy as np 
+from board import Board 
+
+WIDTH = HEIGHT = 512
+DIMENSIONS = 8 
+SQ_SIZE = WIDTH // DIMENSIONS 
+
+IMAGES = {}
+
