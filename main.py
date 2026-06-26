1a4b07d600dd0b7c50a1fc92317d441ef714ec51
Gourav Singh Rajput <gsrajput24sept@gmail.com>
2026-06-26 18:05:36 +0530

    game winow

--- main.py
+++ main.py
@@ -10,3 +10,78 @@ SQ_SIZE = WIDTH // DIMENSIONS
 
 IMAGES = {}
 
+def load_images():
+    pieces = ['wp', 'wR', 'wN', 'wB', 'wQ', 'wK', 'bp', 'bR', 'bN', 'bB', 'bQ', 'bK']
+    for piece in pieces:
+        IMAGES[piece] = pg.image.load(f"images/{piece}.png")
+        IMAGES[piece] = pg.transform.scale(IMAGES[piece], (SQ_SIZE, SQ_SIZE))
+
+
+def draw_board(screen):
+    colors = [pg.Color("white"), pg.Color("gray")]
+
+    for row in range(DIMENSIONS):
+        for col in range(DIMENSIONS):
+
+            color = colors[(row + col) % 2]
+
+            pg.draw.rect(
+                screen,
+                color,
+                (col * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE)
+            )
+
+
+def draw_pieces(screen, board):
+
+    for row in range(DIMENSIONS):
+        for col in range(DIMENSIONS):
+
+            piece = board[row][col]
+
+            if piece != "--":
+
+                screen.blit(
+                    IMAGES[piece],
+                    (col * SQ_SIZE, row * SQ_SIZE)
+                )
+
+
+def main():
+
+    pg.init()
+
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
+
+    clock = pg.time.Clock()
+
+    board = Board()
+
+    load_images()
+
+
+    running = True
+
+    while running:
+
+        for event in pg.event.get():
+
+            if event.type == pg.QUIT:
+                running = False
+
+
+        draw_board(screen)
+
+        draw_pieces(screen, board.board)
+
+
+        pg.display.flip()
+
+        clock.tick(60)
+
+
+    pg.quit()
+
+
+if __name__ == "__main__":
+    main()
