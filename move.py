7ee73158370b10a5b1ef09fc845f7685c5498f3d
Gourav Singh Rajput <gsrajput24sept@gmail.com>
2026-06-27 16:46:06 +0530

    making the move

--- move.py
+++ move.py
@@ -7,10 +7,40 @@ DIMENSIONS = 8
 WIDTH = HEIGHT = 512 
 SQ_SIZE = WIDTH // DIMENSIONS 
 
-x, y = pg.mouse.get_pos()
+board = Board()
 
-row = x // SQ_SIZE
-col = y // SQ_SIZE
+main()
 
-square = (row, col)
+selected_piece = None
+piece_position = None 
+
+for event in pg.event.get():
+
+    if event.type == pg.MOUSEBUTTONDOWN:
+
+        x, y = pg.mouse.get_pos()
+
+        row = x // SQ_SIZE
+        col = y // SQ_SIZE
+
+        # square = (row, col)
+
+        if selected_piece is None:
+
+            selected_piece = board[row][col]
+
+            piece_position = (row, col)
+
+
+        else:
+
+            new_position = (row, col)
+
+            board[piece_position[0]][piece_position[1]] = "--"
+
+            board[new_position[0]][new_position[1]] = selected_piece
+
+            selected_piece = None 
+            piece_position = None
+
 
